bec67d0dc7f22c2680f91e4b1c500a944995171a
Nathan Newport <natern@gmail.com>
2017-11-05 23:55:31 -0500

    Progress up to lesson 125.

---
+++ Source/MyProject/Private/TankPlayerController.cpp
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MyProject.h"
+#include "Tank.h"
+#include "TankPlayerController.h"
+
+ATank* ATankPlayerController::GetControlledTank() const
+{
+	return(Cast<ATank>(GetPawn()));
+}
+
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	ATank* tank = GetControlledTank();
+	if (tank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("TankPlayerController controlling %s"), *(tank->GetName()));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("TankPlayerController couldn't find tank."));
+	}
+	
+}
---
+++ Source/MyProject/Public/TankPlayerController.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/PlayerController.h"
+#include "TankPlayerController.generated.h"
+
+class ATank;
+/**
+ * 
+ */
+UCLASS()
+class MYPROJECT_API ATankPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void BeginPlay() override;
+
+	ATank* GetControlledTank() const;
+};
